db734f17e13e354d2726a411bd9413c910e804d9
LarsVerschoor <larsverschoor2@gmail.com>
2024-01-11 23:27:56 +0100

    Added settings post functionality
    Settings.js now accepts post data to update the settings of users.

--- routes/settings.js
+++ routes/settings.js
@@ -24,11 +24,81 @@ const getSettings = async (req, res, next) => {
   }
 }
 
+const validateInput = (req, res, next) => {
+  const errors = new Array();
+
+  // Regular expression to validate hex colors
+  const hexColorRegex = new RegExp(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/);
+
+  const {
+    color_snake_head,
+    color_snake_body,
+    color_food,
+    color_obstacles
+  } = req.body;
+
+  if (!hexColorRegex.test(color_snake_head)) errors['color_snake_head'] = 'This field only accepts colors in HEX format (#rrggbb or #rgb)';
+  if (!hexColorRegex.test(color_snake_body)) errors['color_snake_body'] = 'This field only accepts colors in HEX format (#rrggbb or #rgb)';
+  if (!hexColorRegex.test(color_food)) errors['color_food'] = 'This field only accepts colors in HEX format (#rrggbb or #rgb)';
+  if (!hexColorRegex.test(color_obstacles)) errors['color_obstacles'] = 'This field only accepts colors in HEX format (#rrggbb or #rgb)';
+
+  if (color_snake_head === '') errors['color_snake_head'] = 'This field cannot be empty';
+  if (color_snake_body === '') errors['color_snake_body'] = 'This field cannot be empty';
+  if (color_food === '') errors['color_food'] = 'This field cannot be empty';
+  if (color_obstacles === '') errors['color_obstacles'] = 'This field cannot be empty';
+
+  if (errors.length === 0) {
+    next();
+    return;
+  }
+
+  res.render('settings', { settings: req.body, errors: errors });
+}
+
+const saveSettings = async (req, res, next) => {
+  const connection = await pool.getConnection();
+  const errors = new Array();
+
+  try {
+    const {
+      color_snake_head,
+      color_snake_body,
+      color_food,
+      color_obstacles
+    } = req.body;
+
+    const query = `
+    UPDATE settings SET
+      color_snake_head = ?,
+      color_snake_body = ?,
+      color_food = ?,
+      color_obstacles = ?
+    WHERE user_id = ?`;
+
+    await connection.execute(query, [
+      color_snake_head,
+      color_snake_body,
+      color_food,
+      color_obstacles,
+      req.userId
+    ])
+
+    next();
+  }
+  catch (error) {
+    errors['general'] = 'An unexpected error occured while saving your settings. Please try again later.';
+    res.render('settings', { settings: req.body, errors: errors });
+  }
+  finally {
+    connection.release();
+  }
+}
+
 router.get('/', checkLoggedIn, authenticate, getSettings, (req, res) => {
-  res.render('settings', {settings: req.settings});
+  res.render('settings', { settings: req.settings, errors: {} });
 })
 
-router.post('/', checkLoggedIn, authenticate, (req, res) => {
+router.post('/', checkLoggedIn, authenticate, validateInput, saveSettings, (req, res) => {
   res.send('<a href="">Return to settings</a><br>Feature still under development')
 })
 
