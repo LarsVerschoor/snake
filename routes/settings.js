59773afcc9abeae3654dcd3194c4c679ded70247
LarsVerschoor <larsverschoor2@gmail.com>
2024-01-11 22:40:29 +0100

    Updated settings page
    Standard colors in settings page are now the colors from the users current settings from the database

--- routes/settings.js
+++ routes/settings.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../includes/database')
 const { checkLoggedIn, authenticate } = require('../includes/authentication');
 
-router.get('/', checkLoggedIn, authenticate, (req, res) => {
-  res.render('settings');
+const getSettings = async (req, res, next) => {
+  const connection = await pool.getConnection();
+  try {
+    const query = 'SELECT * FROM settings WHERE user_id = ?'
+    const [ settings ] = await connection.execute(query, [ req.userId ]);
+    if (settings.length !== 1) {
+      res.redirect('/');
+      return;
+    }
+    req.settings = settings[0];
+    next();
+  }
+  catch (error) {
+    // redirect to error page
+    console.error(error);
+  }
+  finally {
+    connection.release();
+  }
+}
+
+router.get('/', checkLoggedIn, authenticate, getSettings, (req, res) => {
+  res.render('settings', {settings: req.settings});
 })
 
 router.post('/', checkLoggedIn, authenticate, (req, res) => {
