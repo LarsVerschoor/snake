b4d4a3ed37c5063cb0f80f18afec72bcfca1e490
LarsVerschoor <larsverschoor2@gmail.com>
2024-01-12 21:48:17 +0100

    Fixed typos in players.js query

--- routes/players.js
+++ routes/players.js
@@ -20,16 +20,17 @@ const getPlayers = async (req, res, next) => {
       // Get search results from database
       const query = `
       SET @search_term = ?
-      SELECT users.username, users.created_at, COUNT(scores) AS games_played FROM users
+      SELECT users.username, users.created_at, COUNT(scores) AS games_played
+      FROM users
       LEFT JOIN scores ON scores.user_id = users.id
-      WHERE LOWER(username) LIKE LOWER(%?%)
+      WHERE LOWER(username) LIKE LOWER(%@search_term%)
       GROUP BY users.username, users.created_at
       ORDER BY CASE
         WHEN username LIKE @search_term THEN 01
         WHEN LOWER(username) LIKE LOWER(@search_term) THEN 2
         WHEN username LIKE @search_term% THEN 3
         WHEN LOWER(username) LIKE LOWER(@search_term%) THEN 4
-        WHEN username LIKE %search_term% THEN 5
+        WHEN username LIKE %@search_term% THEN 5
         WHEN LOWER(username) LIKE LOWER(%@search_term%) THEN 6
       END;`;
       const [ searchResults ] = await connection.execute(query, [ req.query.q ]);
