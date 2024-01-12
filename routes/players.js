9805540247d54168a1e042928563b0da4422fad7
LarsVerschoor <larsverschoor2@gmail.com>
2024-01-12 21:45:08 +0100

    added player search
    Updated players.js and players.ejs

--- routes/players.js
+++ routes/players.js
@@ -1,9 +1,58 @@
 const express = require('express');
-const router = express.Router();
+
+const pool = require('../includes/database');
 const { checkLoggedIn, authenticate } = require('../includes/authentication');
 
-router.get('/', checkLoggedIn, authenticate, (req, res) => {
-  res.render('players');
+const router = express.Router();
+
+const getPlayers = async (req, res, next) => {
+  try {
+    const connection = await pool.getConnection();
+    try {
+
+      // Validation
+      if (!req.query.q || req.query.q === '' || req.query.q.length > 32) {
+        req.searchResults = [];
+        next();
+        return;
+      }
+
+      // Get search results from database
+      const query = `
+      SET @search_term = ?
+      SELECT users.username, users.created_at, COUNT(scores) AS games_played FROM users
+      LEFT JOIN scores ON scores.user_id = users.id
+      WHERE LOWER(username) LIKE LOWER(%?%)
+      GROUP BY users.username, users.created_at
+      ORDER BY CASE
+        WHEN username LIKE @search_term THEN 01
+        WHEN LOWER(username) LIKE LOWER(@search_term) THEN 2
+        WHEN username LIKE @search_term% THEN 3
+        WHEN LOWER(username) LIKE LOWER(@search_term%) THEN 4
+        WHEN username LIKE %search_term% THEN 5
+        WHEN LOWER(username) LIKE LOWER(%@search_term%) THEN 6
+      END;`;
+      const [ searchResults ] = await connection.execute(query, [ req.query.q ]);
+
+      req.searchResults = searchResults;
+      next();
+    }
+    catch(error) {
+      res.render('players', { results: [], errors: { general: 'An unexpected error occured at the server. Please try again later.' } })
+      console.error(error);
+    }
+    finally {
+      connection.release();
+    }
+  }
+  catch(error) {
+    res.render('players', { results: [], errors: { general: 'An unexpected error occured at the server. Please try again later.' } })
+    console.error(error);
+  }
+}
+
+router.get('/', checkLoggedIn, authenticate, getPlayers, (req, res) => {
+  res.render('players', { errors: {}, results: req.searchResults });
 })
 
 module.exports = router;
